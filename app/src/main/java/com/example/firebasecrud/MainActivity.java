a88ac62780ce77e94588f60022c978167c5dd00c
tadeuaugusto <tadeuaugusto@gmail.com>
2019-02-22 12:02:19 -0300

    Android Toolbar added

--- app/src/main/java/com/example/firebasecrud/MainActivity.java
+++ app/src/main/java/com/example/firebasecrud/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.firebasecrud;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add toolbeer
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Firebase Crud");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
